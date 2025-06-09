1eeda0fd395e652e6d429979b2157993c65c279d
AlbaraaQ <111914470+AlbaraaQ@users.noreply.github.com>
2025-06-10 00:41:46 +0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,92 @@
+﻿Config({
+    "network-name": "الامل اللاسلكية",
+    "service-number": "775950664",
+    "popup-adv-time": 5,
+    "popup-adv-img-type": true,
+    "popup-adv-repeat-after": 120,
+    "login-type": "user",
+    "news-line": "...:::: اهلاً بكم على شبكتنا اللاسلكية تتميز شبكتنا بتقديم مزايا مختلفة لمشتركيها الاعزاء منها السرعة في التصفح ولعب الألعاب ايضاً نقدم خدمة توصيل الانترنت الى المنازل لسنا الوحيدون ولكن نسعى لتقديم خدمة ممتازة على مدار 24 ساعة::::...",
+    "input-type": "text",
+    "input-autocomplete": "on",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 0,
+    "input-only-numbers": 1,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-tel-type-when": 0,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 1,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field": true,
+    "loan-button": false,
+    "redirect-to-esterahah": "",// orWithProtocol: ex, https://google.com leave it blank if dont need esterahah button
+    "redirect-to-mobasher": "",// orWithProtocol: ex, https://google.com leave it blank if dont need mobasher button
+    "app-store-base-url": "",// orWithProtocol: ex, http://192.168.10.10/app/ // leave it blank if locally
+    "profiles": [
+        {
+            "price": "100 ريال",
+            "time": "30 ساعه",
+            "transfer": "300 ميجا",
+            "validity": "5 يوم",
+        },
+
+        {
+            "price": "200 ريال",
+            "time": "60 ساعه",
+            "transfer": "600 ميجا",
+            "validity": "8 يوم",
+        },
+  
+        {
+            "price": "500 ريال",
+            "time": "100 ساعه",
+            "transfer": "1.5 جيجا",
+            "validity": "10 يوم",
+        },
+		
+		{
+            "price": "2500 ريال",
+            "time": "بدون تحديد",
+            "transfer": "10 جيجا",
+            "validity": "30 يوم",
+        },
+		
+		{
+            "price": "4500 ريال",
+            "time": "بدون تحديد",
+            "transfer": "20 جيجا",
+            "validity": "30 يوم",
+        },
+		
+
+    ],
+    "sell-points": [
+        { "name": "مجمع الامل" },
+        { "name": "المعطارة" },
+        { "name": "بقاله محمد عبدالحفيظ" },
+        { "name": "مكتبة المراد" },
+        { "name": "سوبر اسيا" },
+        { "name": "بقاله طوفان اليمن" },
+        { "name": "بقاله البدر" },
+        { "name": "بقاله فايز الوصابي" },
+        { "name": "بقاله خليل الوصابي" },
+        { "name": "بقاله فاضل" },
+        { "name": "بقاله الوصابي" },
+        { "name": "بقاله عاطف" },
+        { "name": "بقاله الشرفي" },
+
+    ]
+})
+
+
+
